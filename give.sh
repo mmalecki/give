8268c2066e97bf060b39f01a8664a426797c33e9
Maciej Małecki <maciej.malecki@notimplemented.org>
2012-02-11 21:33:21 +0100

    [ux api] Basic help

--- give.sh
+++ give.sh
@@ -16,3 +16,23 @@ function give_update {
 function give_checkout {
   cd "$give_dir/src" && git archive --prefix "$1/" --remote node "$1" | tar -xf -
 }
+
+function give_help {
+  echo
+  echo "give - git-based node.js version manager"
+  echo
+  echo "Usage:"
+  echo "  give help        You're staring at it"
+  echo
+}
+
+if [ $# -lt 1 ]; then
+  give_help
+  exit
+fi
+
+case $1 in
+  "help")
+    give_help
+  ;;
+esac
